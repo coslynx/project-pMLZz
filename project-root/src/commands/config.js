99e503679331685141ac65e9257cb5d21541d238
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-24 17:53:44 +0530

    Generated: project-root/src/commands/config.js

---
+++ project-root/src/commands/config.js
@@ -0,0 +1,19 @@
+const { token, youtubeApiKey, spotifyClientId, spotifyClientSecret, soundcloudClientId, soundcloudClientSecret, geniusApiToken, twitchClientId, twitchClientSecret, lastfmApiKey, mongoUri, mysqlHost, mysqlUser, mysqlPassword, mysqlDatabase } = require('dotenv').config().parsed;
+
+module.exports = {
+  token,
+  youtubeApiKey,
+  spotifyClientId,
+  spotifyClientSecret,
+  soundcloudClientId,
+  soundcloudClientSecret,
+  geniusApiToken,
+  twitchClientId,
+  twitchClientSecret,
+  lastfmApiKey,
+  mongoUri,
+  mysqlHost,
+  mysqlUser,
+  mysqlPassword,
+  mysqlDatabase
+};
